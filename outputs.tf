34df06fab00a4b679f19ea1dd3cfacb4b8ebe3ae
tharianjk <tharianjk@users.noreply.github.com>
2025-02-02 15:31:55 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
-
